8e7b9208a0380ef75c8844351ca1016954c0dcec
s-dm <arnfinn.ingebrigtsen@me.com>
2019-03-14 23:35:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  ggApiKey: process.env.GG_API_KEY || 'Your google API key',
-  ggCx: process.env.GG_CX || 'Your search engine ID'
+  ggApiKey: process.env.GG_API_KEY || 'AIzaSyD1lUaRNoDcHDF93H_DAf0UaakGAd7ejrM',
+  ggCx: process.env.GG_CX || '002124380222275982418:7ozt2omzimi'
 }
