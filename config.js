e7d022d901f8eedda4f98dbf663f14b7b2dc3e93
s-dm <arnfinn.ingebrigtsen@me.com>
2019-03-14 23:42:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  ggApiKey: process.env.GG_API_KEY || 'AIzaSyD1lUaRNoDcHDF93H_DAf0UaakGAd7ejrM',
-  ggCx: process.env.GG_CX || '002124380222275982418:7ozt2omzimi'
+  ggApiKey: process.env.GG_API_KEY || '',
+  ggCx: process.env.GG_CX || ''
 }
